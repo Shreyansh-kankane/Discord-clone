3a555be8cb774533a2e017a322972d24db1534d5
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2023-10-12 00:25:43 +0530

    join invitation link

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -30,7 +31,8 @@ export default function RootLayout({
             enableSystem={false}
             storageKey='discord_theme'
           >
-            {children}
+            <ModalProvider/>
+              {children}
           </ThemeProvider>
         </body>
       </html>
